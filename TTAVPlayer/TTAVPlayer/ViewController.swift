4bf25f84966afa7023f6bc5bd1688bb33edeffb2
許仙 <xuxiandaguanren@gmail.com>
2019-03-21 16:22:12 +0800

    创建项目

---
+++ TTAVPlayer/TTAVPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TTAVPlayer
+//
+//  Created by Maiya on 2019/3/21.
+//  Copyright © 2019 Maiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
