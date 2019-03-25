1b8719a6aa66bfd71a4c69e73972bdb62242c990
許仙 <xuxiandaguanren@gmail.com>
2019-03-25 14:29:40 +0800

    自定义PlayerView

---
+++ TTAVPlayer/TTAVPlayer/TTAVPlayer-视频播放器/TTAVPlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  TTAVPlayerView.swift
+//  TTAVPlayer
+//
+//  Created by Maiya on 2019/3/25.
+//  Copyright © 2019 Maiya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TTAVPlayerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
