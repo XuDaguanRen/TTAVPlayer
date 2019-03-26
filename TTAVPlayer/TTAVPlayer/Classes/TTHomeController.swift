2f705bda8fc523655a9985ba1caf4c2830e2dae9
許仙 <xuxiandaguanren@gmail.com>
2019-03-26 10:36:24 +0800

    添加控制器

---
+++ TTAVPlayer/TTAVPlayer/Classes/TTHomeController.swift
@@ -0,0 +1,18 @@
+//
+//  TTHomeController.swift
+//  TTAVPlayer
+//
+//  Created by Maiya on 2019/3/26.
+//  Copyright © 2019 Maiya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TTHomeController: TTBaseController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "首页"
+    }
+}
---
+++ TTAVPlayer/TTAVPlayer/Classes/TTMyController.swift
@@ -0,0 +1,18 @@
+//
+//  TTMyController.swift
+//  TTAVPlayer
+//
+//  Created by Maiya on 2019/3/26.
+//  Copyright © 2019 Maiya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TTMyController: TTBaseController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "我的"
+    }
+}
