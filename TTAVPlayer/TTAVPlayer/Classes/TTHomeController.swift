546fbc1f58cf5aa9ec8c0cfa0246ac82c4bc0b08
許仙 <xuxiandaguanren@gmail.com>
2019-03-27 11:08:31 +0800

    添加测试代码

--- TTAVPlayer/TTAVPlayer/Classes/TTHomeController.swift
+++ TTAVPlayer/TTAVPlayer/Classes/TTHomeController.swift
@@ -19,6 +19,18 @@ class TTHomeController: TTBaseController {
     }
     
    fileprivate func setupHomeUI() -> Void {
-        
+    //1.从mainBundle获取test.mp4的具体路径
+    //        let paths: String? = "https://lymanli-1258009115.cos.ap-guangzhou.myqcloud.com/video/sample/sample-video2.mp4"
+    //        let paths = Bundle.main.path(forResource: "01-课程安排", ofType: "mp4")
+    //        path =  "///var/containers/Bundle/Application/A9B82066-0405-484D-8BF0-64BC438B0D4A/Touch.app/01-%E8%AF%BE%E7%A8%8B%E5%AE%89%E6%8E%92.mp4"
+//    let videoName = "01-课程安排.mp4"
+    
+    let path = Bundle.main.path(forResource: "01-课程安排", ofType: "mp4")
+    
+    let ttPlayer = TTAVPlayerView.init(frame: CGRect(x: 0, y: 80, width: kScreemWidth, height: 180))
+    ttPlayer.urlString = path!
+    
+    view.addSubview(ttPlayer)
+    
     }
 }
